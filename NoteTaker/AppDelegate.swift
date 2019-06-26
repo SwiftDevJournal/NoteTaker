d25e9167cf3c9e23a2545ecaf3b33b6f33341175
Mark Szymczyk <mark@meandmark.com>
2019-06-26 15:13:36 -0400

    Initial Commit

---
+++ NoteTaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoteTaker
+//
+//  Created by mark on 6/26/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
