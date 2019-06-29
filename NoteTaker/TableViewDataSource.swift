9230b6b7fd989406049163c8f2a5de723eb50526
Mark Szymczyk <mark@meandmark.com>
2019-06-29 15:04:36 -0400

    Add the required data source and delegate methods for the table view: numberOfRows and viewForTableColumn.

--- NoteTaker/TableViewDataSource.swift
+++ NoteTaker/TableViewDataSource.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2019 Swift Dev Journal. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+extension TableViewController: NSTableViewDelegate, NSTableViewDataSource {
+    
+    func numberOfRows(in aTableView: NSTableView) -> Int {
+        if let splitViewController = parent as? SplitViewController {
+            return splitViewController.notes.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        if let cellView = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "TitleColumn"), owner: self) as? NSTableCellView {
+            
+            if let splitViewController = parent as? SplitViewController {
+                cellView.textField?.stringValue = splitViewController.notes[row].title
+            }
+            
+            return cellView
+        }
+        
+        return nil
+    }
+}
