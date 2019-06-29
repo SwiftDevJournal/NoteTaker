5d9d6469f61f3c061f28876f30a4b8e0496663e3
Mark Szymczyk <mark@meandmark.com>
2019-06-29 14:56:13 -0400

    Add properties to the split view controller for the list of notes and the child view controllers.

--- NoteTaker/SplitViewController.swift
+++ NoteTaker/SplitViewController.swift
@@ -10,6 +10,17 @@ import Cocoa
 
 class SplitViewController: NSSplitViewController {
 
+    // I'm storing the notes in the split view controller because it's a simple app and the split view controller contains the list of notes. You could create a NoteController class to manage the array of notes to avoid polluting the view controller with code that doesn't manage the split view.
+    var notes = [Note]()
+    
+    var tableViewController: TableViewController? {
+        return children.first as? TableViewController
+    }
+    
+    var textViewController: TextViewController? {
+        return children[1] as? TextViewController
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
