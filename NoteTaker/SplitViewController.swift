cc442a73352951a81958a26507121291716022f3
Mark Szymczyk <mark@meandmark.com>
2019-06-29 14:46:09 -0400

    Add a class file for the split view controller to the project.

---
+++ NoteTaker/SplitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplitViewController.swift
+//  NoteTaker
+//
+//  Created by mark on 6/29/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+class SplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
