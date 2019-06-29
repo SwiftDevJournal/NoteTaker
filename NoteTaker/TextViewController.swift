b28dbf68339f4f0fba39f6e8a3c80c3995008ea6
Mark Szymczyk <mark@meandmark.com>
2019-06-28 23:04:00 -0400

    Add file for the text view controller to the project.

---
+++ NoteTaker/TextViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TextViewController.swift
+//  NoteTaker
+//
+//  Created by mark on 6/28/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+class TextViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
