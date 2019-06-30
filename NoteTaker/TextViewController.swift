5ea3cb613bc297668cb2226b7d1889a0d21775fe
Mark Szymczyk <mark@meandmark.com>
2019-06-29 22:55:56 -0400

    Change the text view's contents when the table view selection changes.
    Save the text view contents when the text changes in the text view.

--- NoteTaker/TextViewController.swift
+++ NoteTaker/TextViewController.swift
@@ -8,13 +8,42 @@
 
 import Cocoa
 
-class TextViewController: NSViewController {
+class TextViewController: NSViewController, NSTextDelegate {
 
     @IBOutlet var textView: NSTextView!
+    
+    var currentNoteLocation = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        registerForNotifications()
     }
     
+    func registerForNotifications() {
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(TextViewController.textDidChange(_:)),
+                           name: NSText.didChangeNotification,
+                           object: textView)
+    }
+    
+    func changeTextViewContents(selectedRow: Int) {
+        if let splitViewController = parent as? SplitViewController {
+            let selectedNote = splitViewController.notes[selectedRow]
+            // Fill text view with the selected note contents
+            textView.textStorage?.setAttributedString(selectedNote.contents)
+            currentNoteLocation = selectedRow
+        }
+    }
+    
+    func saveTextViewContents() {
+        if let splitViewController = parent as? SplitViewController {
+            // The note will have the text view's contents.
+            splitViewController.notes[currentNoteLocation].contents = textView.attributedString()
+        }
+    }
+    
+    func textDidChange(_ notification: Notification) {
+        saveTextViewContents()
+    }
 }
