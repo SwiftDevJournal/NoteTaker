37d3dbb02b4095a3458f9889745fb3a344ccd887
Mark Szymczyk <mark@meandmark.com>
2019-06-29 21:45:13 -0400

    Implement the delegate function textShouldEndEditing in the table view controller so a note's title can be edited from the table view.

--- NoteTaker/TableCellEditing.swift
+++ NoteTaker/TableCellEditing.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2019 Swift Dev Journal. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+extension TableViewController: NSControlTextEditingDelegate {
+    
+    // I have to implement NSControlTextEditingDelegate's textShouldEndEditing method to change the note title by editing its name in the table view.
+    func control(_ control: NSControl, textShouldEndEditing fieldEditor: NSText) -> Bool {
+        let selectedRow = tableView.selectedRow
+        if let splitViewController = parent as? SplitViewController {
+            splitViewController.notes[selectedRow].title = fieldEditor.string
+            return true
+        }
+        
+        return false
+    }
+}
