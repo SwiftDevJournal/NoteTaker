11a863bfa389beaf7fb233f6a31cf39c5fe2c050
Mark Szymczyk <mark@meandmark.com>
2019-07-22 20:58:44 -0400

    Add functions to write notes to a Data object and read notes from a Data object.

--- NoteTaker/Note.swift
+++ NoteTaker/Note.swift
@@ -11,4 +11,15 @@ import Foundation
 struct Note {
     var title: String = ""
     var contents: NSMutableAttributedString = NSMutableAttributedString(string: "")
+    
+    func write() -> Data? {
+        let range = NSRange(location: 0, length: contents.length)
+        return contents.rtf(from: range)
+    }
+    
+    mutating func read(data: Data) {
+        if let fileContents = NSAttributedString(rtf: data, documentAttributes: nil) {
+            contents = NSMutableAttributedString(attributedString: fileContents)
+        }
+    }
 }
