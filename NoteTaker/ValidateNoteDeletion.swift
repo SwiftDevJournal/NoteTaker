ff11b60efa92afd81b02070ab70f378a035d1c64
Mark Szymczyk <mark@meandmark.com>
2019-08-09 15:32:15 -0400

    Validate the user interface item so the Delete Note menu item is enabled only if there are notes to delete.
    The Delete Note toolbar button is still enabled. I may need it to conform to the NSValidatedUserInterfaceItem protocol.

--- NoteTaker/ValidateNoteDeletion.swift
+++ NoteTaker/ValidateNoteDeletion.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2019 Swift Dev Journal. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+// Validate the Delete Note menu item and toolbar button so they are disabled if the notes array is empty.
+
+extension SplitViewController {
+    
+    override func validateUserInterfaceItem(_ item: NSValidatedUserInterfaceItem) -> Bool {
+        switch item.action {
+            
+        case #selector(deleteNote(_:))?:
+            // Put your real test here.
+            return !(notes.isEmpty)
+            
+        default:
+            return true
+        }
+    }
+}
