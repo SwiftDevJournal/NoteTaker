78ba4416834f3d931f7dc866f1fcb92bf78e25ae
Mark Szymczyk <mark@meandmark.com>
2019-08-09 15:54:57 -0400

    Add a ToolbarButton subclass of NSToolbarItem so I can disable the Delete Note button if the notes array is empty.

---
+++ NoteTaker/ToolbarButton.swift
@@ -0,0 +1,13 @@
+//
+//  ToolbarButton.swift
+//  NoteTaker
+//
+//  Created by mark on 8/9/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+class ToolbarButton: NSToolbarItem {
+
+}
