53f2c027238caaa68b6d8901d55c3aba73e72196
Mark Szymczyk <mark@meandmark.com>
2019-08-09 16:07:33 -0400

    Override the validate function to disable the Delete Note toolbar button when there are no notes.
    I got the code from an answer on Stack Overflow: stackoverflow.com/questions/42470645/nstoolbaritem-validation-in-relevant-controller
    
    The palette label does not change its enabled/disabled state. Fix that.

--- NoteTaker/ToolbarButton.swift
+++ NoteTaker/ToolbarButton.swift
@@ -10,4 +10,23 @@ import Cocoa
 
 class ToolbarButton: NSToolbarItem {
 
+    // This code is from the answer I found on Stack Overflow
+    // NSToolbarItem validation in relevant controller
+    // URL: https:// stackoverflow.com/questions/42470645/nstoolbaritem-validation-in-relevant-controller
+    override func validate() {
+        if let control = self.view as? NSControl,
+            let action = self.action,
+            let validator = NSApp.target(forAction: action, to: self.target, from: self) as AnyObject? {
+        
+            switch validator {
+            case let validator as NSUserInterfaceValidations:
+                control.isEnabled = validator.validateUserInterfaceItem(self)
+            default:
+                control.isEnabled = validator.validateToolbarItem(self)
+            }
+            
+        } else {
+            super.validate()
+        }
+    }
 }
